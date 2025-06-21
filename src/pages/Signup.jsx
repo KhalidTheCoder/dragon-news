1841d2bda9fbcd54decc7e1de680567452a69413
Khalidahsan007 <khalidasan2407@gmail.com>
2025-06-21 21:29:17 +0600

    signup functinality added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,39 +1,74 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Signup = () => {
+  const { createUser, setUser } = use(AuthContext);
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    createUser(email, password)
+      .then((res) => {
+        const user = res.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
         <h2 className="text-2xl font-semibold text-center">
           Register your account
         </h2>
-        <div className="card-body">
+        <form onSubmit={handleSignUp} className="card-body">
           <fieldset className="fieldset">
-
-
-
             <label className="label">Name</label>
-            <input type="text" className="input" placeholder="Your Name" />
-
-
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Your Name"
+              required
+            />
 
             <label className="label">Photo URL</label>
-            <input type="text" className="input" placeholder="Photo URL" />
-
-
-
+            <input
+              name="photo"
+              type="text"
+              className="input"
+              placeholder="Photo URL"
+              required
+            />
 
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-
-
-
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
 
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-            
-            <button className="btn btn-neutral mt-4">Sign Up</button>
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+
+            <button type="submit" className="btn btn-neutral mt-4">
+              Sign Up
+            </button>
             <p className="font-semibold text-center pt-5">
               Already Have An Account ?{" "}
               <Link className="text-secondary" to="/auth/login">
@@ -41,7 +76,7 @@ const Signup = () => {
               </Link>
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
